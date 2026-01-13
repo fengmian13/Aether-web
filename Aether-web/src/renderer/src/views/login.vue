5b13b251f66192f0ec1fb870e7043806bb31a580
wlei <fengmian13@163.com>
2026-01-13 20:15:34 +0800

    框架修改

---
+++ Aether-web/src/renderer/src/views/login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="login-pannel">
+    <div class="title drag">Anther</div>
+    <div class="login-form">
+      <div class="error-msg"></div>
+      <el-form :model="formData" :rules="rules" ref="formDataRef" label-width="0px" @submit.prevent>
+        <!--input输入-->
+        <el-form-item prop="email">
+          <el-input size="large" clearable placeholder="请输入邮箱" v-model.trim="formData.email">
+            <template #prefix>
+              <span class="iconfont icon-email"></span>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            size="large"
+            show-password
+            clearable
+            placeholder="请输入密码"
+            v-model.trim="formData.password"
+          >
+            <template #prefix>
+              <span class="iconfont icon-password"></span>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="checkcode">
+          <el-input
+            size="large"
+            clearable
+            placeholder="请输入验证码"
+            v-model.trim="formData.checkcode"
+          >
+            <template #prefix>
+              <span class="iconfont icon-checkcode"></span>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="">
+          <el-button type="primary" class="login-btn" @click="login()">登录</el-button>
+        </el-form-item>
+        <div class="bottom-link">
+          <span>没有账号？</span>
+        </div>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, getCurrentInstance, nextTick } from 'vue'
+const { proxy } = getCurrentInstance()
+
+const formData = ref({})
+const formDataRef = ref()
+const rules = {
+  title: [{ required: true, message: '请输入内容' }]
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
