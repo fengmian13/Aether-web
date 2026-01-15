5b0935d2b7b82d81c8f8bb8aafc072bc6ffe75a4
wlei <fengmian13@163.com>
2026-01-16 00:23:04 +0800

    ``` feat(WinOp): 添加窗口操作组件
    新增 WinOp.vue 组件，提供窗口置顶、最小化、最大化和关闭功能。
    支持自定义显示按钮，通过 IPC 与主进程通信实现窗口操作。
    包含响应式状态管理，支持回调事件处理。
    ```

---
+++ Aether-web/src/renderer/src/components/WinOp.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="win-op no-drag">
+    <!-- 按钮 -->
+    <div
+      v-if="showSetTop"
+      :class="['iconfont icon-top', isTop ? 'win-top' : '']"
+      @click="top"
+      :title="isTop ? '取消置顶' : '置顶'"
+    ></div>
+    <!-- 最小化 -->
+    <div v-if="showMin" class="iconfont icon-min" @click="minimize" title="最/小化"></div>
+    <!-- 最大化 -->
+    <div
+      v-if="showMax"
+      @click="maximize"
+      :class="['iconfont', isMax ? 'icon-maximize' : 'icon-max']"
+      :title="isMax ? '向下还原' : '最大化'"
+    ></div>
+    <!-- 关闭 -->
+    <div v-if="showClose" class="iconfont icon-close" @click="close" title="关闭"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, getCurrentInstance, nextTick, onMounted } from 'vue'
+const { proxy } = getCurrentInstance()
+
+const props = defineProps({
+  showSetTop: {
+    type: Boolean,
+    default: true
+  },
+  showMin: { type: Boolean, default: true },
+  showMax: {
+    type: Boolean,
+    default: true
+  },
+  showClose: { type: Boolean, default: true }, //关闭类型0:关闭，1:隐藏
+  closeType: { type: Number, default: 1 }
+})
+
+const isMax = ref(false)
+const isTop = ref(false)
+
+onMounted(() => {
+  isMax.value = false
+  isTop.value = false
+})
+
+const winOp = (action, data) => {
+  window.ipcRenderer.send('winTitleOp', { action, data })
+}
+
+const emit = defineEmits(['closeCallback'])
+const close = () => {
+  winOp('close', { closeType: props.closeType })
+  emit('closeCallback')
+}
+
+const minimize = () => {
+  winOp('minimize')
+}
+
+const maximize = () => {
+  if (isMax.value) {
+    winOp('unmaximize')
+    isMax.value = false
+  } else {
+    winOp('maximize')
+    isMax.value = true
+  }
+}
+
+const top = () => {
+  isTop.value = !isTop.value
+  winOp('top', { top: isTop.value })
+}
+</script>
+
+<style lang="scss" scoped>
+.win-op {
+  top: 0px;
+  right: 0px;
+  position: absolute;
+  z-index: 1;
+  overflow: hidden;
+  border-radius: 0px 3px 0px 0;
+  .iconfont {
+    float: left;
+    font-size: 12px;
+    color: #101010;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+    height: 25px;
+    align-items: center;
+    padding: 0px 10px;
+    &:hover {
+      background: #ddd;
+    }
+  }
+  .icon-close {
+    &:hover {
+      background: #fb7373;
+      color: #fff;
+    }
+    .win-top {
+      background: #ddd;
+      color: #07c160;
+    }
+  }
+  .icon-top.win-top {
+    color: #ff6f00; // 橙色文字（你可以改成背景色等）
+    // 如果你想加背景色，可以取消下面注释：
+    // background: rgba(255, 111, 0, 0.1);
+  }
+}
+</style>
